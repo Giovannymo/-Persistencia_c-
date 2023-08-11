ea2ff88dd5c13125a4ed3b408df06e563f58dcd9
Giovanny Moreno <giovannysocas@gmail.com>
2023-08-10 23:24:13 -0500

    add: Category List

--- Env.cs
+++ Env.cs
@@ -4,8 +4,11 @@ namespace productos_tienda;
     {
         private static string fileName = "products.json";
         private static List<Product> products = new List<Product>();
+        private static List<Category> categories = new List<Category>();
         public static string FileName {get => fileName; set => fileName = value; }
         public static List<Product> Products {get => products; set => products = value;}
-    
+        public static List<Category> Categories {get => categories; set => categories = value;}
+
+        
         
     }
